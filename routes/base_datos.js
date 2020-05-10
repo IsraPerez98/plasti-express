fa945ea0cc69c1690b759a3d1a9a8e5c978da398
IsraPerez98 <isra1998isra@gmail.com>
2020-05-09 21:59:55 -0400

    cambios menores de optimizacion

--- routes/base_datos.js
+++ routes/base_datos.js
@@ -1,17 +1,17 @@
 //Import the mongoose module
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 //Set up default mongoose connection
 
 //datos de esto se escriben en el archivo .env, ver documentacion dotenv'
 //var mongoDB = `mongodb://${process.env.MONGODB_USUARIO}:${process.env.MONGODB_CONTRASENA}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PUERTO}/${process.env.MONGODB_DATABASE}`;
 // no vamos a considerar usuario y contraseña todavia
-var mongoDB = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PUERTO}/${process.env.MONGODB_DATABASE}`;
+const mongoDB = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PUERTO}/${process.env.MONGODB_DATABASE}`;
 console.log(mongoDB);
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Get the default connection
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
